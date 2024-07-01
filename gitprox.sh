0ad6af5b5cad28d976917bb853519787f4806422
Modd <modd@Endling.endling.com>
2024-07-01 13:18:33 +0300

    deleted gitprox

--- gitprox.sh
+++
@@ -1,7 +0,0 @@
-echo "# proxy-xtractor" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/modd3/proxy-xtractor.git
-git push -u origin main
